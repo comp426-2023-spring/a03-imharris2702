8e82b15a127f49da3a07dcecbac5faf1d5a3c5d2
Isaiah Harris <imharris@unc.edu>
2023-02-22 17:20:14 -0500

    function working with no input

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+
+function rps(shot) {
+	if (shot === undefined) {
+		const choice = Math.floor(Math.random() * 3); 
+		switch (choice) {
+			case 0:
+				return {player: "rock"};
+			case 1:
+				return {player: "paper"};
+			default:
+				return {player: "scissors"};
+		}
+	}
+}
+
+const test = rps();
+console.log(test)
